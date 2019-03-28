956e4909496676d614e6fc553aa35d54acbf8c32
mrstephenneal <stephen.neal14@gmail.com>
2019-03-28 13:05:29 -0400

    ADD test suit and S3 Sync unittest

---
+++ tests/test_s3_sync.py
@@ -0,0 +1,20 @@
+import unittest
+from looptools import Timer
+from awsutils.s3 import S3
+
+
+S3_BUCKET = 'awsutils-tests'
+
+
+class TestManipulateInsert(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.s3 = S3(S3_BUCKET)
+
+    @Timer.decorator
+    def test_s3_sync(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
