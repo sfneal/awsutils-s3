da338fa89407c836a39f9f51727759684bbb732f
mrstephenneal <stephen.neal14@gmail.com>
2019-03-28 16:02:49 -0400

    ADD printer function for pretty printing test results

--- tests/test_s3_list.py
+++ tests/test_s3_list.py
@@ -6,6 +6,11 @@ from awsutils.s3 import S3
 S3_BUCKET = 'awsutils-tests'
 
 
+def printer(header, body):
+    """Pretty print lists for visual check that correct output was returned."""
+    print('\n{0}:\n'.format(header.upper()) + '\n'.join('\t{0}'.format(b) for b in body))
+
+
 class TestManipulateInsert(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
@@ -15,6 +20,7 @@ class TestManipulateInsert(unittest.TestCase):
     def test_s3_list_buckets(self):
         buckets = self.s3.buckets
         self.assertIsInstance(buckets, list)
+        printer('Available S3 Buckets', buckets)
 
 
 if __name__ == '__main__':
