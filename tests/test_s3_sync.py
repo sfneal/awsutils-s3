fd81ea613c8ded2ae4e60d0233fef370b2016360
mrstephenneal <stephen.neal14@gmail.com>
2019-03-29 10:44:36 -0400

    ADD assertion to test_s3_sync method

--- tests/test_s3_sync.py
+++ tests/test_s3_sync.py
@@ -1,10 +1,14 @@
 import unittest
 import os
 from looptools import Timer
+from dirutility import DirPaths
 from awsutils.s3 import S3
 
 
 S3_BUCKET = 'awsutils-tests'
+TEST_PATH = 'awsutils/s3'
+LOCAL_BASE = os.path.dirname(os.path.dirname(__file__))
+LOCAL_PATH = os.path.join(LOCAL_BASE, 'awsutils', 's3')
 
 
 class TestManipulateInsert(unittest.TestCase):
@@ -16,6 +20,12 @@ class TestManipulateInsert(unittest.TestCase):
     def test_s3_sync(self):
         target = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'awsutils')
         self.s3.sync(target)
+
+        s3_files = self.s3.list(recursive=True)
+        local_files = [os.path.join('awsutils', path) for path in DirPaths(os.path.join(LOCAL_BASE, 'awsutils')).walk()]
+        # printer('Remote S3 Files', s3_files)
+        # printer('Local Files', local_files)
+        self.assertEqual(set(s3_files), set(local_files))
 
 
 if __name__ == '__main__':
