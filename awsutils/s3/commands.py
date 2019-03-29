efedca84e89df9ba4201792593d185c09ebc389f
mrstephenneal <stephen.neal14@gmail.com>
2019-03-29 10:55:46 -0400

    ADD remove method to S3Commands

--- awsutils/s3/commands.py
+++ awsutils/s3/commands.py
@@ -1,6 +1,6 @@
 class S3Commands:
     @staticmethod
-    def copy(uri1, uri2, recursive=None, include=None, exclude=None):
+    def copy(uri1, uri2, recursive=False, include=None, exclude=None):
         """
         Copy file(s)/folder(s) from one S3 bucket location to another
 
@@ -11,14 +11,28 @@ class S3Commands:
         :param exclude: Exclude all files or objects from the command that matches the specified pattern
         :return: Command string
         """
-        cmd = 'aws s3 cp {uri1} {uri2}'.format(uri1=uri1, uri2=uri2)
-        if recursive:
-            cmd += ' --recursive'
-        if include:
-            cmd += ' --include "{0}"'.format(include)
-        if exclude:
-            cmd += ' --exclude "{0}"'.format(exclude)
-        return cmd
+        cmd = 'aws s3 cp {uri1} {uri2}'
+        cmd += ' --recursive' if recursive else ''
+        cmd += ' --include "{0}"'.format(include) if include else ''
+        cmd += ' --exclude "{0}"'.format(exclude) if exclude else ''
+        return cmd.format(uri1=uri1, uri2=uri2)
+
+    @staticmethod
+    def remove(uri, recursive=False, include=None, exclude=None):
+        """
+        Delete an S3 object from a bucket.
+
+        :param uri: S3 object uri
+        :param recursive: Recursively copy all files within the directory
+        :param include: Don't exclude files or objects in the command that match the specified pattern
+        :param exclude: Exclude all files or objects from the command that matches the specified pattern
+        :return: Command string
+        """
+        cmd = 'aws s3 rm {uri}'
+        cmd += ' --recursive' if recursive else ''
+        cmd += ' --include "{0}"'.format(include) if include else ''
+        cmd += ' --exclude "{0}"'.format(exclude) if exclude else ''
+        return cmd.format(uri=uri)
 
     @staticmethod
     def sync(source_path, destination_uri, delete=False, acl='private'):
