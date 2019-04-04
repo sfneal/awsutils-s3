568aa2d0085829016f189528a50b22819b297f29
mrstephenneal <stephen.neal14@gmail.com>
2019-04-04 14:13:07 -0400

    FIX issue with importing helper functions

--- awsutils/s3/__init__.py
+++ awsutils/s3/__init__.py
@@ -1,4 +1,4 @@
-from awsutils.s3.s3 import S3
+from awsutils.s3.s3 import S3, url_validator, url_extract, key_extract
 
 
-__all__ = ['S3']
+__all__ = ['S3', 'url_validator', 'url_extract', 'key_extract']
