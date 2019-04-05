a93f447b8ed380a46aa7b3229dfd20c341a8a6a5
mrstephenneal <stephen.neal14@gmail.com>
2019-04-05 11:08:20 -0400

    FIX setUpClass's

--- tests/test_s3_buckets.py
+++ tests/test_s3_buckets.py
@@ -14,7 +14,6 @@ class TestS3Buckets(unittest.TestCase):
     @Timer.decorator
     def test_list_buckets(self):
         buckets = self.s3.buckets
-        # printer('Available S3 Buckets', buckets)
         self.assertIsInstance(buckets, list)
 
 
@@ -23,11 +22,15 @@ class TestS3BucketCreate(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
+        if cls.s3.bucket_name in cls.s3.buckets:
+            cls.s3.delete_bucket()
+
+    @classmethod
+    def tearDownClass(cls):
         cls.s3.delete_bucket()
 
     @Timer.decorator
     def test_create(self):
-        self.s3.bucket_name = self.s3.bucket_name + '2'
         self.s3.create_bucket()
         self.assertTrue(self.s3.bucket_name in self.s3.buckets)
 
@@ -37,11 +40,15 @@ class TestS3delete(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
+        if cls.s3.bucket_name not in cls.s3.buckets:
+            cls.s3.create_bucket()
+
+    @classmethod
+    def tearDownClass(cls):
         cls.s3.create_bucket()
 
     @Timer.decorator
     def test_delete(self):
-        self.s3.bucket_name = self.s3.bucket_name + '2'
         self.s3.delete_bucket()
         self.assertFalse(self.s3.bucket_name in self.s3.buckets)
 
