12c91ab4dd7ea583c34037b74d38d61ef9c4f0ac
sfneal <stephen.neal14@gmail.com>
2024-05-17 12:46:55 -0400

    FIX TEST_PATH constant to use test data directory in VCS to avoid GH action failures

--- tests/_config.py
+++ tests/_config.py
@@ -2,6 +2,6 @@ import os
 
 
 S3_BUCKET = 'py-awsutils-tests'
-TEST_PATH = 'dist'
+TEST_PATH = os.path.join('tests', 'data', 'dist')
 LOCAL_BASE = os.path.dirname(os.path.dirname(__file__))
 LOCAL_PATH = os.path.join(LOCAL_BASE, TEST_PATH)
