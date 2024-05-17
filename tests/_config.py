be18cd6ce7324de5ee52a06ab8c613007f8c4bb0
sfneal <stephen.neal14@gmail.com>
2024-05-17 13:05:34 -0400

    FIX issues with bucket creation/deletion in test setup/tear down

--- tests/_config.py
+++ tests/_config.py
@@ -2,6 +2,6 @@ import os
 
 
 S3_BUCKET = 'py-awsutils-tests'
-TEST_PATH = os.path.join('tests', 'data', 'dist')
+TEST_PATH = 'data'
 LOCAL_BASE = os.path.dirname(os.path.dirname(__file__))
 LOCAL_PATH = os.path.join(LOCAL_BASE, TEST_PATH)
--- tests/_testcase.py
+++ tests/_testcase.py
@@ -8,3 +8,12 @@ from tests._config import S3_BUCKET
 class TestCase(unittest.TestCase):
     _bucket = S3_BUCKET + '-' + str(uuid4())
     s3 = S3(_bucket, quiet=True)
+
+    @classmethod
+    def setUpClass(cls):
+        if cls.s3.bucket_name not in cls.s3.buckets:
+            cls.s3.create_bucket()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.s3.delete_bucket(force=True)
